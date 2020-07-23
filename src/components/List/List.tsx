ef48db55d3cbe8b69ed48120f964e29157e62af7
krzysztof <krzysztofkrych2@gmail.com>
2020-07-23 17:38:07 +0200

    information if list is empty

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 
 export interface Props<T = []> {
     items: T[];
     displayData: (item: T, index: number) => React.ReactNode;
 }
 
+const StyledDiv = styled.div`
+    margin-top: 1rem;
+`
+
 const List = <T extends object>({items, displayData}: Props<T>) => (
     <div>
-        {items.map(displayData)}
+        {items && items.length ? items.map(displayData) : <StyledDiv>Customers list is empty.</StyledDiv>}
     </div>
 );
 
