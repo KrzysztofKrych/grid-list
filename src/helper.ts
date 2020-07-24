16064a3749a213427e310c635f11d32362ffb93d
krzysztof <krzysztofkrych2@gmail.com>
2020-07-24 10:13:08 +0200

    sort list by click on table-head and fix for filtering values

--- src/helper.ts
+++ src/helper.ts
@@ -2,10 +2,20 @@ import Customer from "./models/Customer";
 
 export const emailRegex = /\S+@\S+\.\S+/;
 
-export const filterCustomersByValue = (customers: Customer[], value: string) => (
-    customers.filter(customer => 
-        customer.name.includes(value) || customer.email.includes(value) || customer.phone.includes(value)
-    )
+export const filterCustomersByValue = (customers: Customer[], value: string) => {
+    const LowerCasedValue = value.toLowerCase();
+    return (
+        customers.filter(customer => 
+            customer.name.toLowerCase().includes(LowerCasedValue) || 
+            customer.email.toLowerCase().includes(LowerCasedValue) || 
+            customer.phone.toLowerCase().includes(LowerCasedValue)
+        )
+    );
+};
+
+
+export const sortByStringValue = <T>(sorter: (value: T) => string, list: T[]) =>(
+    list.sort((a,b) => sorter(a).localeCompare(sorter(b)))
 );
 
 export const validators = {
@@ -13,4 +23,4 @@ export const validators = {
     isValidCustomer: (name: string, email: string): boolean => (
         !!name && !!name.length && validators.isValidEmail(email)
     )
-}
+};
